7e003e339f5d7d8c041d42a2933df742cbce03b1
*Yoon <*sscer93@naver.com*>
2017-12-28 15:22:25 +0900

    second commit

--- src/main/java/kr/or/nextit/board/controller/BoardController.java
+++ src/main/java/kr/or/nextit/board/controller/BoardController.java
@@ -24,6 +24,8 @@ import kr.or.nextit.common.file.service.FileItemService;
 import kr.or.nextit.common.util.PagingUtil;
 import kr.or.nextit.member.model.Member;
 
+
+// 주석 추가
 @Controller
 @RequestMapping("/board")
 public class BoardController {
